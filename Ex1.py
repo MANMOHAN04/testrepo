394ea4d388f4aec99dc7353cad616ccd6f92f0f8
MANMOHAN04 <90182368+MANMOHAN04@users.noreply.github.com>
2022-03-08 16:00:20 +0530

    Add files via upload

---
+++ Ex1.py
@@ -0,0 +1,13 @@
+a=200
+b=20
+#a = input("Enter the value of a:")
+#b = input("Enter the value of b:")
+#c=int(a)+int(b)
+#print(c)
+
+if a>b:
+    print("a:{0} is greater than b:{1}".format(a,b))
+elif a==b:
+    print("a is equal to b")
+else:
+    print("a is less than b")
